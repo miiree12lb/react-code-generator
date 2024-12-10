d347f1eb8167a59a10175730fc64ded91d561f94
Mireia <66952824+miiree12lb@users.noreply.github.com>
2024-12-10 20:18:26 +0100

    leftnav text elements

--- src/LeftNav.jsx
+++ src/LeftNav.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 // @ts-ignore
 import "./css/leftnav.css";
 
-function LeftNav() {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+function LeftNav({ isCollapsed, setIsCollapsed }) {
+    const [isTextOpen, setIsTextOpen] = useState(false);
 
-    const toggleCollapse = () => {
+
+    const handleCollapseToggle = () => {
         setIsCollapsed(!isCollapsed);
     };
 
@@ -17,10 +18,54 @@ function LeftNav() {
             }}
         >
             <div id="left-text-container" style={{ display: isCollapsed ? "none" : "block" }}>
-                <h4>Text Elements</h4>
+                <h4
+                    onClick={() => setIsTextOpen(!isTextOpen)}
+                >
+                    Text Elements{" "}
+                    <i className={`fa ${!isTextOpen ? "fa-caret-down" : "fa-caret-up"}`} />
+                </h4>
+                {isTextOpen && <div className="dropdown">
+                    <div className="item" draggable>
+                        <p>Title 1</p>
+                        <h1>Title 1</h1>
+                    </div>
+                    
+                    <div className="item" draggable>
+                        <p>Title 2</p>
+                        <h2>Title 2</h2>
+                    </div>
+
+                    <div className="item" draggable>
+                        <p>Title 3</p>
+                        <h3>Title 3</h3>
+                    </div>
+
+                    <div className="item" draggable>
+                        <p>Title 4</p>
+                        <h4>Title 4</h4>
+                    </div>
+
+                    <div className="item" draggable>
+                        <p>Title 5</p>
+                        <h5>Title 5</h5>
+                    </div>
+
+                    <div className="item" draggable>
+                        <p>Title 6</p>
+                        <h6>Title 6</h6>
+                    </div>
+
+                    <div className="item" draggable>
+                        <p>Text</p>
+                        <h6>Text</h6>
+                    </div>
+                </div>}
+
+                
                 <h4>Lists</h4>
             </div>
-            <div id="collapse-arrow" onClick={toggleCollapse}>
+
+            <div id="collapse-arrow" onClick={handleCollapseToggle}>
                 <i
                     id="caret"
                     className={`fa ${isCollapsed ? "fa-caret-right" : "fa-caret-left"}`}
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./Footer.jsx";
 import Nabvar from "./Navbar.jsx";
 import LeftNav from "./LeftNav.jsx";
 // @ts-ignore
-import './css/root.css';
+import "./css/root.css";
 
 function Root() {
     const [theme, setTheme] = useState("light");
+    const [isLeftCollapsed, setIsLeftCollapsed] = useState(false);
 
     useEffect(() => {
         document.documentElement.setAttribute("data-theme", theme);
@@ -18,16 +19,30 @@ function Root() {
         <BrowserRouter>
             <Nabvar theme={theme} setTheme={setTheme} />
 
-            
-            <LeftNav />
-            
+            <LeftNav isCollapsed={isLeftCollapsed} setIsCollapsed={setIsLeftCollapsed} />
 
-            <div id="view">
-                
+            <div
+                id="view"
+                style={{
+                    marginLeft: isLeftCollapsed ? "30px" : "300px", // Adjust margin based on collapsed state
+                }}
+            >
+                <div id="input-area">
+                    {/* Input area content */}
+                </div>
+
+                <div id="output-area">
+                    {/* Output area content */}
+                </div>
             </div>
 
-            <div id="footer">
-                <Footer theme={theme}/>
+            <div
+                id="footer"
+                style={{
+                    marginLeft: isLeftCollapsed ? "30px" : "300px", // Adjust footer margin to align with the navbar
+                }}
+            >
+                <Footer theme={theme} />
             </div>
         </BrowserRouter>
     );
@@ -37,3 +52,5 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
     createRoot(rootElement).render(<Root />);
 }
+
+export default Root;
